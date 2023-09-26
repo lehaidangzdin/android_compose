aca5ac74711bf273ddc403d911f115c676e661d6
lehaidangzdin <dang2002ns@gmail.com>
2023-09-26 14:00:13 +0700

    optimize import

--- app/src/main/java/com/lhd/samcenter/MainActivity.kt
+++ app/src/main/java/com/lhd/samcenter/MainActivity.kt
@@ -3,17 +3,11 @@ package com.lhd.samcenter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.lhd.samcenter.navigation.NavigationAppHost
-import com.lhd.samcenter.presention.home.HomeViewModel
-import com.lhd.samcenter.presention.ui.theme.SamcenterTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
